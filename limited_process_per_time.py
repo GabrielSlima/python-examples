f377bc8306b333d4707bc53beb576772e8cb5db6
Gabriel Lima <34993129+GabrielSlima@users.noreply.github.com>
2021-02-09 17:43:01 -0300

    Create limited_process_per_time.py

---
+++ limited_process_per_time.py
@@ -0,0 +1,46 @@
+from multiprocessing import Process
+import time
+
+
+def get_name(name):
+    print("Processed: {}".format(name))
+    time.sleep(5)
+
+
+def update_status(jobs):
+    status = []
+    for job in jobs.values():
+        status.append(job.is_alive())
+    return status
+
+
+def get_curves_async(curves, MAX_POOLS, each_request_timeout):
+    end_item = MAX_POOLS
+    start_item = 0
+    total_processed_items = []
+    while start_item <= len(curves) - 1:
+        jobs = {}
+        for current_index, curve in enumerate(curves[start_item: end_item]):
+            process = Process(target=get_name, args=(curve,))
+            process.start()
+            total_processed_items.append(curve)
+            jobs[curve] = process
+
+        update_status(jobs)
+        healthcheck_time = 0
+        there_is_running_jobs = True
+        while there_is_running_jobs in update_status(jobs):
+            time.sleep(5)
+            healthcheck_time += 5
+            if healthcheck_time == each_request_timeout:
+                raise Exception("Couldn't get all responses: {}".format(jobs))
+        start_item = len(total_processed_items)
+        end_item += MAX_POOLS
+        print("Processed items: {}".format(jobs.keys()))
+        print("Total processed items: {}".format(total_processed_items))
+
+
+if __name__ == '__main__':
+    names = ['Gabriel', "Caio", "Adriano", "Jagunço", "Marcelo", "Jonas", "Ana", "Samuel", "Messias"]
+    jobs = []
+    get_curves_async(names, 5, 300)
