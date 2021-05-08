a0b6f376287461480e31acaff612b715f1a7c0bd
Gabriel Lima <34993129+GabrielSlima@users.noreply.github.com>
2021-05-08 16:21:04 -0300

    Update private-static-data-members.py

--- classes/private-static-data-members.py
+++ classes/private-static-data-members.py
@@ -1,8 +1,8 @@
-# PRIVATE CLASS VARIABLES(STATIC) CAN'T BE ACCESSED OUTSIDE OF IT'S CLASS WITHOUT
-# A PUBLIC INTERFACE
+# PRIVATE CLASS VARIABLES(STATIC) CAN'T BE ACCESSED OUTSIDE OF IT'S CLASS WITHOUT A PUBLIC INTERFACE
 
-# THE PUBLIC INTERFACES TO ACCESS THE CLASSES'S STATIC DATA MEMBERS ARE STATIC. THEY DO NOT OPERATE OVER AN OBJECT.
-# CLASSES'S PUBLIC INTERFACES ARE NOT ATTACHED TA SPECIFFIC OBJECT, SO THE self/this, WHICH CONTAINS THE OBJECTS's REFERENCE, IS NOT PRESENT
+# THE PUBLIC INTERFACES TO ACCESS THE CLASSES'S STATIC DATA MEMBERS ARE ALSO STATIC, WHICH MEANS THAT THEY ARE ALSO MEMBERS OF THE CLASS. 
+# THEY DO NOT OPERATE OVER AN OBJECT.
+# CLASSES'S PUBLIC INTERFACES ARE NOT ATTACHED TA SPECIFIC OBJECT, SO THE self/this, WHICH CONTAINS THE OBJECTS's REFERENCE, IS NOT PRESENT
 
 # WE CAN'T ACCESS INSTANCE VARIABLES/FUNCTIONS THROUGHT CLASSES'S/STATIC FUNCTIONS BCZ THE CLASS DOESN'T HAVE
 # THE REFERENCE OF ANY OF IT'S INSTANCES/OBJECTS
@@ -30,11 +30,11 @@ class Car:
     def max_wheels(self, amount): #BOUND/ATTACHED TO A OBJECT
         self.__MAX_WHEELS = amount
     
-    def change_class_wheels(self, amount):
+    def change_class_wheels(self, amount): #BOUND/ATTACHED TO A OBJECT
         __MAX_WHEELS = amount # __MAX_WHEELS IS A LOCAL VARIABLE
         print(__MAX_WHEELS)
     
-    def change_class_wheels_using_name(self, amount):
+    def change_class_wheels_using_name(self, amount): #BOUND/ATTACHED TO A OBJECT
         Car.__MAX_WHEELS = amount
         
 
@@ -43,11 +43,12 @@ lambo = Car("Lamborghini", "Gallardo")
 lambo.max_wheels(2)
 print(lambo.wheels()) #2
 print(Car.default_amount_of_wheels()) #4
+
 lambo.change_class_wheels(56) # DOES NOTHIG TO THE OBJECT OR CLASS
 print(Car.default_amount_of_wheels()) #4
 print(lambo.wheels()) #2
 
-lambo.change_class_wheels_using_name(56) # CHANGE THE CLASS'S STATE
+lambo.change_class_wheels_using_name(56) # CHANGE THE CLASS'S STATE/VARIABLE
 print(Car.default_amount_of_wheels()) #56
 print(lambo.wheels()) #2
 print(lambo.default_amount_of_wheels()) #56
