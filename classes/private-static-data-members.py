523c51cc048cc68524beae01c80f771def3d4e52
gabrielSlima <santos.biel5@gmail.com>
2021-05-08 16:15:47 -0300

    Add classes and objects members example

--- classes/private-static-data-members.py
+++ classes/private-static-data-members.py
@@ -1,44 +1,54 @@
-import pprint
 # PRIVATE CLASS VARIABLES(STATIC) CAN'T BE ACCESSED OUTSIDE OF IT'S CLASS WITHOUT
 # A PUBLIC INTERFACE
 
 # THE PUBLIC INTERFACES TO ACCESS THE CLASSES'S STATIC DATA MEMBERS ARE STATIC. THEY DO NOT OPERATE OVER AN OBJECT.
 # CLASSES'S PUBLIC INTERFACES ARE NOT ATTACHED TA SPECIFFIC OBJECT, SO THE self/this, WHICH CONTAINS THE OBJECTS's REFERENCE, IS NOT PRESENT
 
+# WE CAN'T ACCESS INSTANCE VARIABLES/FUNCTIONS THROUGHT CLASSES'S/STATIC FUNCTIONS BCZ THE CLASS DOESN'T HAVE
+# THE REFERENCE OF ANY OF IT'S INSTANCES/OBJECTS
+
+# TO ACCESS A CLASS'S MEMBER THROUGHT A INSTANCE FUNCTION MEMBER WHE HAVE TO USE THE CLASS'S REFERENCE
+# <CLASS_NAME>.<MEMBER_NAME>
+
 class Car:
     __MAX_WHEELS = 4
-    def __init__(self, type, model):
+    def __init__(self, type, model): #BOUND/ATTACHED TO A OBJECT
         self.model = model
         self.type = type
     
     @staticmethod
-    def default_amount_of_wheels():
+    def default_amount_of_wheels(): #UNBOUND/NOT ATTACHED TO A OBJECT
         return Car.__MAX_WHEELS
 
     @staticmethod
-    def set_default_amount_of_wheels(amount):
+    def set_default_amount_of_wheels(amount): #UNBOUND/NOT ATTACHED TO A OBJECT
         Car.__MAX_WHEELS  = amount
-
-#print(Car.__MAX_WHEELS) => AttributeError: type object 'Car' has no attribute '__MAX_WHEELS'
-Car.__MAX_WHEELS = 2
-print(pprint.pprint(Car.__dict__))
+        
+    def wheels(self): #BOUND/ATTACHED TO A OBJECT
+        return self.__MAX_WHEELS
+    
+    def max_wheels(self, amount): #BOUND/ATTACHED TO A OBJECT
+        self.__MAX_WHEELS = amount
+    
+    def change_class_wheels(self, amount):
+        __MAX_WHEELS = amount # __MAX_WHEELS IS A LOCAL VARIABLE
+        print(__MAX_WHEELS)
+    
+    def change_class_wheels_using_name(self, amount):
+        Car.__MAX_WHEELS = amount
+        
 
 print(Car.default_amount_of_wheels()) #4
-lambo = Car("Lamborghini", "Gallardo") #{'model': 'Gallardo', 'type': 'Lamborghini'}
-print(lambo.default_amount_of_wheels()) #4
+lambo = Car("Lamborghini", "Gallardo")
+lambo.max_wheels(2)
+print(lambo.wheels()) #2
+print(Car.default_amount_of_wheels()) #4
+lambo.change_class_wheels(56) # DOES NOTHIG TO THE OBJECT OR CLASS
+print(Car.default_amount_of_wheels()) #4
+print(lambo.wheels()) #2
 
-Car.set_default_amount_of_wheels(2)
-
-print(Car.default_amount_of_wheels()) #2
-print(lambo.default_amount_of_wheels()) #2
-lambo = Car("Lamborghini", "Gallardo") #{'model': 'Gallardo', 'type': 'Lamborghini'}
-print(lambo.default_amount_of_wheels()) #2
-
-print(pprint.pprint(Car.__dict__)) #mappingproxy({'_Car__MAX_WHEELS': 2, ..)
-print(lambo.__dict__) #mappingproxy({'_Car__MAX_WHEELS': 2, ..)
-
-#print(lambo.__MAX_WHEELS) => AttributeError: type object 'Car' has no attribute '__MAX_WHEELS'
-lambo.__MAX_WHEELS = 2
-lambo.non_existing_attribute_on_template = 2 
-print(lambo.__dict__) #{'model': 'Gallardo', 'type': 'Lamborghini', '__MAX_WHEELS': 2, 'non_existing_attribute_on_template': 2}
-print(lambo.__MAX_WHEELS) #2
+lambo.change_class_wheels_using_name(56) # CHANGE THE CLASS'S STATE
+print(Car.default_amount_of_wheels()) #56
+print(lambo.wheels()) #2
+print(lambo.default_amount_of_wheels()) #56
+print(Car.__MAX_WHEELS) # AttributeError: type object 'Car' has no attribute '__MAX_WHEELS'
--- classes/static-data-members.py
+++ classes/static-data-members.py
@@ -1,16 +1,23 @@
-# CLASS VARIABLES (STATIC VARS/DATA MEMBERS) BECOMES INSTANCE VARIABLES 
-# WHEN A NEW OBJECT IS BROUGHT TO EXISTANCE
+# STATIC DATA MEMBERS: THERE IS ONLY ONE COPY OF THEM FOR EVERY INSTANCE/OBJECT.
+
+# CLASS VARIABLES (STATIC VARS/DATA MEMBERS) BECOMES INSTANCE VARIABLES
+# WHEN A NEW OBJECT IS BROUGHT TO EXISTANCE IN PYTHON
+# BUT THE CLASS MEMBER CAN BE ACCESS THROUGHT <CLASS_NAME>.<VAR_NAME>
+ 
 
 class Car:
     MAX_WHEELS = 4
     def __init__(self, type, model):
         self.model = model
         self.type = type
+    
+    def show_class_member(self):
+        return Car.MAX_WHEELS
 
-print(Car.MAX_WHEELS) #STATIC VARIABLE
-
+print(Car.MAX_WHEELS) #STATIC VARIABLE - 4
 lambo = Car("Lamborghini", "Gallardo")
 lambo.MAX_WHEELS = 2 #INSTANCE VARIABLE
-
-print(lambo.MAX_WHEELS)
+print(lambo.MAX_WHEELS) #2
+print(Car.MAX_WHEELS) #4
+print(lambo.show_class_member()) #4
 print(Car.type) #AttributeError: type object 'Car' has no attribute 'type'
